c8ba15e370e3c596586fa3054044d25de8b5bcee
Veronika <veronika.obrtelova181@gmail.com>
2023-10-30 20:12:03 +0100

    commit

--- findElementsTest.py
+++ findElementsTest.py
@@ -1,7 +1,30 @@
+import time
+
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webelement import WebElement
 
 service_obj = Service(executable_path="/Users/veronikaobrtelova/Downloads/chromedriver-mac-arm64/chromedriver")
 driver = webdriver.Chrome(service=service_obj)
 
 driver.get("https://rahulshettyacademy.com/dropdownsPractise/")
+
+driver.find_element(By.ID, "autosuggest").send_keys("ind") #Zobrazí se vyskakovací okno
+time.sleep(2)
+countries = driver.find_elements(By.CSS_SELECTOR, "li[class='ui-menu-item'] a")
+print(len(countries))
+
+for country in countries:
+    if country.text == "India":
+        country.click()
+        break
+
+#print(driver.find_element(By.ID, "autosuggest").text)
+assert driver.find_element(By.ID, "autosuggest").get_attribute("value") == "India"  #asertace = srovnání
+
+
+
+
+
+
