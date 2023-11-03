8cc3deb4f5f15a0c9be6765b2b0ba3d88f024f2c
Veronika <veronika.obrtelova181@gmail.com>
2023-11-03 18:23:56 +0100

    commit

---
+++ PythonTesting/pytestsDemo/test_demo1.py
@@ -0,0 +1,10 @@
+#jakýkoli soubor pytest by měl začínat na test_ nebo končit na _test
+#Název metody pytest by měl začínat na test
+#Jakýkoli kód by měl být zabalen pouze do metody
+
+def test_firstProgram():
+   print("Hello")
+
+def test_Greet():
+    print("Good Morning")
+
---
+++ PythonTesting/pytestsDemo/test_demo2.py
@@ -0,0 +1,8 @@
+def test_firstProgram():
+    msg = "Hello"
+    assert msg == "Hi", "Test failed because strings do not match"
+
+def test_SecondProgram():
+    a = 4
+    b = 6
+    assert a+2 == 6, "Addition do not match"
