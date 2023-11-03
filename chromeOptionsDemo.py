df4d6a9bec501fbdb95ff6eff8a8de053132f706
Veronika <veronika.obrtelova181@gmail.com>
2023-11-03 13:41:43 +0100

    commit

--- chromeOptionsDemo.py
+++ chromeOptionsDemo.py
@@ -1,11 +1,16 @@
-from selenium.webdriver.chrome import webdriver
+from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.select import Select
 
-chrome_options = webdriver.chromeOptions()
+chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument("--start-maximized")
+chrome_options.add_argument("headless")
+chrome_options.add_argument("--ignore-certificate-errors")
 
 service_obj = Service(executable_path="/Users/veronikaobrtelova/Downloads/chromedriver-mac-arm64/chromedriver")
-driver = webdriver.Chrome(executable_path="\\chromedriver.exe")
+driver = webdriver.Chrome(service=service_obj)
 
 
 driver.get("https://rahulshettyacademy.com/angularpractice/")
+
+print(driver.title)
