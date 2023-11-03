ed8c50d0e71b7e1fcb327444c87f51caa695a6a4
Veronika <veronika.obrtelova181@gmail.com>
2023-11-03 19:55:32 +0100

    commit

--- PythonTesting/pytestsDemo/test_demo1.py
+++ PythonTesting/pytestsDemo/test_demo1.py
@@ -1,10 +1,23 @@
 #jakýkoli soubor pytest by měl začínat na test_ nebo končit na _test
 #Název metody pytest by měl začínat na test
 #Jakýkoli kód by měl být zabalen pouze do metody
+#název metody by měl mít smysl
+#-k znamená provedení názvů metod, -s odhlášení odhlášení - v znamená metada více informací
+#můžete spustit konkrétní soubor pomocí py.test <název souboru>
+#můžete označit tag testy @pytest.mark.smoke a běh pomocí -m
+#můžete přeskočit testy s @pytest.mark.skip
+import pytest
+@pytest.fixture()
+def setup():
+    print("I will be executing first")
+    yield
+    print("I will executed last")
 
-def test_firstProgram():
+@pytest.mark.smoke
+def test_firstProgram(setup):
    print("Hello")
 
-def test_Greet():
+@pytest.mark.xfail
+def test_SecondGreetCreditCard():
     print("Good Morning")
 
---
+++ PythonTesting/pytestsDemo/test_fixtureDemo.py
@@ -0,0 +1,7 @@
+import pytest
+
+
+
+
+def test_fixtureDemo(setup):
+    print("i will execute steps in fixtureDemo method")
